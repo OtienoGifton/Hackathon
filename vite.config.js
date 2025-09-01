647207b4badb7d1a335ecc010ff4ee5c43923d7a
Gifton Otieno <otienogifton2004@gmail.com>
2025-09-01 16:06:26 +0300

    adjusted dist

--- vite.config.js
+++ vite.config.js
@@ -3,8 +3,22 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  base: '',
   server: {
     port: 3000,
     open: true
+  },
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+    sourcemap: false,
+    rollupOptions: {
+      output: {
+        manualChunks: undefined,
+        assetFileNames: 'assets/[name]-[hash][extname]',
+        chunkFileNames: 'assets/[name]-[hash].js',
+        entryFileNames: 'assets/[name]-[hash].js'
+      }
+    }
   }
 })
